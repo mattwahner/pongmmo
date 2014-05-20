41d22c18c3cd78994ffa30a1deeb36d84c119917
Matt Wahner <ohaloxreacho@gmail.com>
2014-05-20 08:08:19 -0500

    Updated packet tests

--- PacketTest/src/main/Test.java
+++ PacketTest/src/main/Test.java
@@ -6,25 +6,42 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Test {
+public class Test implements Runnable{
+
+	private boolean running = false;
+	private static PrintWriter out;
+	private static BufferedReader in;
 
 	public static void main(String[] args){
 		Socket echoSocket;
-		PrintWriter out;
-		BufferedReader in;
 		try {
 			echoSocket = new Socket("127.0.0.1", 7777);
 			out = new PrintWriter(echoSocket.getOutputStream(), true);
 			in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
 			System.out.println("Connection established");
-			String userInput;
-			while((userInput = in.readLine()) != null){
-				out.println(userInput);
-				System.out.println("echo: " + in.readLine());
-			}
+			new Test().start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public void start(){
+		running = true;
+		new Thread(this).run();
+	}
+
+	public void run() {
+		String userInput;
+		while(running){
+			try {
+				if((userInput = in.readLine()) != null){
+					out.println(userInput);
+					System.out.println("echo: " + in.readLine());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
--- ServerTest/src/main/ServerTest.java
+++ ServerTest/src/main/ServerTest.java
@@ -7,7 +7,12 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServerTest {
+public class ServerTest implements Runnable{
+	
+	private static PrintWriter out;
+	private static BufferedReader in;
+	
+	private boolean running = false;
 	
 	public static void main(String[] args){
 		try {
@@ -15,19 +20,41 @@ public class ServerTest {
 			System.out.println("Server started");
 			Socket clientSocket = serverSocket.accept();
 			System.out.println("Connection accepted");
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			System.out.println("Connection established");
 			out.println("test");
 			System.out.println("First packet sent");
-			String inputLine;
-			while((inputLine = in.readLine()) != null){
-				System.out.println(inputLine);
-				out.println("test");
-				System.out.println("Packet sent");
-			}
+			new ServerTest().start();
+			System.out.println("Exited while");
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+		System.out.println("Exit");
+	}
+	
+	public void start(){
+		running = true;
+		new Thread(this).run();
+	}
+
+	public void run() {
+		while(running){
+			out.println("test");
+			System.out.println("Packet sent");
+			String inLine;
+			try {
+				if((inLine = in.readLine()) != null){
+					System.out.println("Recieved: " + inLine);
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
